dc660149d6f646eeba27a15587d8db7d5cd4a6b3
Pratyushanand26 <anandpratyush26@gmail.com>
2025-09-26 11:42:52 +0530

    v1 complete

--- backend.py
+++ backend.py
@@ -81,9 +81,13 @@ checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 
+
+#Nodes
 graph.add_node("chat_node", chat_node)
 graph.add_node("tools",tool_node)
 
+
+#Edges
 graph.add_edge(START, "chat_node")
 graph.add_conditional_edges("chat_node",tools_condition)
 graph.add_edge('tools', 'chat_node')
