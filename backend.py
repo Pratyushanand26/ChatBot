655dfc6b77644ef1ac816a58b20c3e29e96e7829
Pratyushanand26 <anandpratyush26@gmail.com>
2025-09-26 09:22:56 +0530

    added backend

---
+++ backend.py
@@ -0,0 +1,37 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langchain.schema import HumanMessage
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+workflow = graph.compile(checkpointer=checkpointer)
+
+initial_state = ChatState(messages=[HumanMessage(content="What is the capital of India?")])
+
+print(workflow.invoke(initial_state)["messages"][-1].content)
